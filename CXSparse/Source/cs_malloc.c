27f607f5ec65d4651f8b70a5a7e2d971b123c1f9
David Heffernan <david.heffernan@orcina.com>
2025-04-10 11:31:55 +0100

    use hostalloc

--- CXSparse/Source/cs_malloc.c
+++ CXSparse/Source/cs_malloc.c
@@ -2,17 +2,18 @@
 // CXSparse, Copyright (c) 2006-2022, Timothy A. Davis. All Rights Reserved.
 // SPDX-License-Identifier: LGPL-2.1+
 #include "cs.h"
+#include "hostalloc.h"
 
 /* wrapper for malloc */
 void *cs_malloc (CS_INT n, size_t size)
 {
-    return (SuiteSparse_config_malloc (CS_MAX (n,1) * size)) ;
+    return (host_malloc (CS_MAX (n,1) * size)) ;
 }
 
 /* wrapper for calloc */
 void *cs_calloc (CS_INT n, size_t size)
 {
-    return (SuiteSparse_config_calloc (CS_MAX (n,1), size)) ;
+    return (host_calloc (CS_MAX (n,1), size)) ;
 }
 
 /* wrapper for free */
@@ -21,7 +22,7 @@ void *cs_free (void *p)
     if (p)
     {
         /* free p if it is not already NULL */
-        SuiteSparse_config_free (p) ;
+        host_free (p) ;
     }
     return (NULL) ;         /* return NULL to simplify the use of cs_free */
 }
@@ -31,7 +32,7 @@ void *cs_realloc (void *p, CS_INT n, size_t size, CS_INT *ok)
 {
     void *pnew ;
     /* realloc the block */
-    pnew = SuiteSparse_config_realloc (p, CS_MAX (n,1) * size) ;
+    pnew = host_realloc (p, CS_MAX (n,1) * size) ;
     *ok = (pnew != NULL) ;                  /* realloc fails if pnew is NULL */
     return ((*ok) ? pnew : p) ;             /* return original p if failure */
 }
